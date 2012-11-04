260e17a78df81f15ebd51e59a800fc81e971b66e
Gavin <gavin.lin.asd@gmail.com>
2012-11-04 14:02:54 -0500

    Initial empty commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GavinlinCom::Application.config.secret_token = '8c2986bf77b574d3c2a86f345f48710675f9cb050546fbf91141a393f991f5268b027eca715ef78c072f04938583c68d73cf8c2c0e23aaec492054d18415dbbd'
