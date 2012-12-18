372a0b1b334760f98c0fad5a7ae2120841cd148c
Gavin <gavin.lin.asd@gmail.com>
2012-12-18 02:26:00 -0500

    Clear directory

--- db/migrate/20081224020432_remove_openid_authority_from_comment.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOpenidAuthorityFromComment < ActiveRecord::Migration
-  def up
-    remove_column :comments, :author_openid_authority
-  end
-
-  def down
-    add_column :comments, :author_openid_authority, :string, :null => false
-  end
-end
