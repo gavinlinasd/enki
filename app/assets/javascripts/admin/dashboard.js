bcb9e1cd89b79a43512962136cc1444868cd6f67
Gavin <gavin.lin.asd@gmail.com>
2013-01-03 20:38:36 -0500

    Added a few background texture image. Improve the admin dashboard style with bootstrap. Next step: add bootstrap to other admin page

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -7,19 +7,6 @@ $(document).ready(function (){
   if ($('#recent-comments').length) {
     $('.comment-body').hide();
 
-    $('.comment-link').click (function() {
-      comment_body_id = '#comment-body-' + extractId($(this));
-
-      $('.comment-body').not(comment_body_id).hide();
-      $(comment_body_id).toggle();
-
-      return false;
-    })
-
-    $(document).click(function() {
-      $('.comment-body').hide();
-    });
-
     asyncUndoBehaviour();
 
     $('form.delete-item').submit(function () {
