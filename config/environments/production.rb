35081f63e6f8597aa133b8551aad38258a2d2f5d
Gavin <gavin.lin.asd@gmail.com>
2013-01-21 22:23:25 -0500

    Changed rake cache config to store entity into memcached too

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Enki::Application.configure do
   # memcache config (Don't understand the :entitystore variable)
   config.action_dispatch.rack_cache = {
     :metastore    => Dalli::Client.new,
-    :entitystore  => 'file:tmp/cache/rack/body',
+    :entitystore  => Dalli::Client.new,
+    # :entitystore  => 'file:tmp/cache/rack/body',
     :allow_reload => false
   }
 
